483bfaf70bf18041827b1307bfa2ac2f665b0708
Andrew Vy <andrew@andrewvy.com>
2016-12-01 10:58:33 -0800

    :zap: [Puzzle 1] Allow for arbitrary inputs

--- lib/solutions/puzzle_1.ex
+++ lib/solutions/puzzle_1.ex
@@ -9,10 +9,8 @@ end
 defmodule AOC.Solutions.Puzzle1 do
   alias AOC.Solutions.Puzzle1.Position
 
-  @input "R1, R1, R3, R1, R1, L2, R5, L2, R5, R1, R4, L2, R3, L3, R4, L5, R4, R4, R1, L5, L4, R5, R3, L1, R4, R3, L2, L1, R3, L4, R3, L2, R5, R190, R3, R5, L5, L1, R54, L3, L4, L1, R4, R1, R3, L1, L1, R2, L2, R2, R5, L3, R4, R76, L3, R4, R191, R5, R5, L5, L4, L5, L3, R1, R3, R2, L2, L2, L4, L5, L4, R5, R4, R4, R2, R3, R4, L3, L2, R5, R3, L2, L1, R2, L3, R2, L1, L1, R1, L3, R5, L5, L1, L2, R5, R3, L3, R3, R5, R2, R5, R5, L5, L5, R2, L3, L5, L2, L1, R2, R2, L2, R2, L3, L2, R3, L5, R4, L4, L5, R3, L4, R1, R3, R2, R4, L2, L3, R2, L5, R5, R4, L2, R4, L1, L3, L1, L3, R1, R2, R1, L5, R5, R3, L3, L3, L2, R4, R2, L5, L1, L1, L5, L4, L1, L1, R1"
-
-  def solve() do
-    @input
+  def solve(input) do
+    input
     |> split_input()
     |> perform()
     |> calculate_distance_from_point({0, 0})
@@ -26,9 +24,7 @@ defmodule AOC.Solutions.Puzzle1 do
 
   defp perform(list_of_instructions) when is_list(list_of_instructions) do
     list_of_instructions
-    |> Enum.reduce(%Position{}, fn(instruction, state) ->
-      perform(instruction, state)
-    end)
+    |> Enum.reduce(%Position{}, &perform/2)
   end
 
   defp perform("R" <> tiles, state), do: rotate(state, :r) |> move(String.to_integer(tiles))
