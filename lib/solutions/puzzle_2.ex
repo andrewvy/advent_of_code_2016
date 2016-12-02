f045dbf036ccea57dc5d1bd786ab2220bd908e97
Andrew Vy <andrew@andrewvy.com>
2016-12-02 10:07:44 -0800

    :zap: [Puzzle 2] Add Puzzle 2 MapSet solution

---
+++ lib/solutions/puzzle_2.ex
@@ -0,0 +1,106 @@
+defmodule AOC.Solutions.Puzzle2.Position do
+  @moduledoc """
+  This struct contains fields for keeping track of the current direction,
+  and the coordinates of the current position.
+  """
+
+  @type t :: %__MODULE__{
+    current_direction: :north | :east | :south | :west,
+    x: integer(),
+    y: integer()
+  }
+
+  defstruct [
+    current_direction: :north,
+    visited: MapSet.new(),
+    visited_history: [],
+    x: 0,
+    y: 0
+  ]
+end
+
+defmodule AOC.Solutions.Puzzle2 do
+  alias AOC.Solutions.Puzzle2.Position
+
+  @moduledoc """
+  This is the solution for Puzzle 2, which utilizes function head pattern matching to
+  control how to change the position based on the rotation and the number of tiles moved.
+  """
+
+  @spec solve(String.t) :: integer()
+  def solve(input) do
+    input
+    |> split_input()
+    |> perform()
+    |> get_first_visited_twice()
+    |> calculate_distance_from_point({0, 0})
+  end
+
+  @spec calculate_distance_from_point(Position.t | {integer(), integer()}, {integer(), integer()}) :: integer()
+  def calculate_distance_from_point(%Position{x: x, y: y}, {point_x, point_y}) do
+    calculate_distance_from_point({x, y}, {point_x, point_y})
+  end
+  def calculate_distance_from_point({x, y}, {point_x, point_y}) do
+    abs(x - point_x) + abs(y - point_y)
+  end
+
+  @spec get_first_visited_twice(Position.t) :: {integer(), integer()}
+  def get_first_visited_twice(%Position{visited_history: visited_history}) do
+    List.last(visited_history)
+  end
+
+  # ---
+
+  @spec split_input(String.t) :: String.t
+  defp split_input(string), do: string |> String.split(", ")
+
+  @spec perform([] | [String.t] | String.t) :: Position.t
+  defp perform(list_of_instructions) when is_list(list_of_instructions) do
+    list_of_instructions
+    |> Enum.reduce(%Position{}, &perform/2)
+  end
+  defp perform("R" <> tiles, state), do: rotate(state, :r) |> move_multiple(String.to_integer(tiles))
+  defp perform("L" <> tiles, state), do: rotate(state, :l) |> move_multiple(String.to_integer(tiles))
+
+  @spec rotate(Position.t, :r | :l) :: Position.t
+  defp rotate(%Position{current_direction: :north} = state, :r), do: %Position{state | current_direction: :east}
+  defp rotate(%Position{current_direction: :north} = state, :l), do: %Position{state | current_direction: :west}
+  defp rotate(%Position{current_direction: :east} = state, :r), do: %Position{state | current_direction: :south}
+  defp rotate(%Position{current_direction: :east} = state, :l), do: %Position{state | current_direction: :north}
+  defp rotate(%Position{current_direction: :south} = state, :r), do: %Position{state | current_direction: :west}
+  defp rotate(%Position{current_direction: :south} = state, :l), do: %Position{state | current_direction: :east}
+  defp rotate(%Position{current_direction: :west} = state, :r), do: %Position{state | current_direction: :north}
+  defp rotate(%Position{current_direction: :west} = state, :l), do: %Position{state | current_direction: :south}
+
+  @spec move_multiple(Position.t, integer()) :: Position.t
+  defp move_multiple(state, tiles) do
+    1..tiles
+    |> Enum.reduce(state, fn(_, acc) ->
+      acc
+      |> move()
+      |> check_if_visited_before()
+      |> add_position_to_visited()
+    end)
+  end
+
+  @spec move(Position.t) :: Position.t
+  defp move(%Position{current_direction: :north} = state), do: %Position{state | y: state.y + 1}
+  defp move(%Position{current_direction: :east} = state), do: %Position{state | x: state.x + 1}
+  defp move(%Position{current_direction: :south} = state), do: %Position{state | y: state.y - 1}
+  defp move(%Position{current_direction: :west} = state), do: %Position{state | x: state.x - 1}
+
+  @spec add_position_to_visited(Position.t) :: Position.t
+  defp add_position_to_visited(position) do
+    %Position{position | visited: position.visited |> MapSet.put({position.x, position.y})}
+  end
+
+  @spec check_if_visited_before(Position.t) :: Position.t
+  defp check_if_visited_before(position) do
+    case MapSet.member?(position.visited, {position.x, position.y}) do
+      true ->
+        %Position{position | visited_history: [{position.x, position.y} | position.visited_history]}
+      false ->
+        position
+    end
+  end
+end
